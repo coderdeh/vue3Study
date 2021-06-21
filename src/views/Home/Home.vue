70cdcdba54caf304d405323e25f4d68eb39ee8c9
coderdeh <erhui@dlz-tech.com>
2021-06-21 16:15:37 +0800

    feat: add Bg

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="h-screen w-screen">
-    <svg-icon name="test"></svg-icon>
-  </div>
+  <div class="h-screen w-screen bg-home-theme"></div>
 </template>
 
 <script lang="ts">
-import SvgIcon from '@/components/SvgIcon.vue'
 import { defineComponent, reactive, toRefs } from 'vue'
 
 export default defineComponent({
-  components: { SvgIcon },
   setup() {
     const state = reactive({})
     return {
